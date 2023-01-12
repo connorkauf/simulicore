28a4d763387dbe392000e9e7e993fe0b951ba8cd
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-01-12 20:09:24 +0200

    Configuration updates
      Add execute permissions build.sh & config.sh
      Add .gitignore
      Add doxyfile
      Change test.?pp to mark documentation
      File build.sh accepts args

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 EXEC_DIR="$PWD"
 ROOT_DIR=`dirname $0`
 cd ${ROOT_DIR}/build
-ninja && ninja install
+ninja $@ && ninja install
 cd ${EXEC_DIR}
 exit 0
--- config.sh
+++ config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ROOT_DIR=`dirname $0`
-rm -f ${ROOT_DIR}/install/lib/* 
-rm -rf ${ROOT_DIR}/build/* ${ROOT_DIR}/build.ninja_*
+rm -rf ${ROOT_DIR}/build/*
+rm -rf ${ROOT_DIR}/build.ninja_*
+rm -rf ${ROOT_DIR}/install/*
+rm -rf ${ROOT_DIR}/docs/*
 cmake -GNinja -S ${ROOT_DIR} -B ${ROOT_DIR}/build
 exit 0
