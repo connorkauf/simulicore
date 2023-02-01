a83e5cdd1381d5c78767d6bd985b4740203d201c
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-02-01 16:10:40 +0200

    Add new files for dns checks

---
+++ cla3p/src/bulk/dns_checks.hpp
@@ -0,0 +1,26 @@
+#ifndef CLA3P_DNS_CHECKS_HPP_
+#define CLA3P_DNS_CHECKS_HPP_
+
+#include "../types.hpp"
+#include "../error_internal.hpp"
+
+/*-------------------------------------------------*/
+namespace cla3p {
+namespace dns {
+namespace bulk {
+/*-------------------------------------------------*/
+
+// simple check of input
+status_t input_consistency_status(prop_t ptype, uint_t m, uint_t n, const void *a, uint_t lda);
+
+// throws exception on error
+// returns if pattern is lower
+bool check(prop_t ptype, uint_t m, uint_t n, const void *a, uint_t lda);
+
+/*-------------------------------------------------*/
+} // namespace bulk
+} // namespace dns
+} // namespace cla3p
+/*-------------------------------------------------*/
+
+#endif // CLA3P_DNS_CHECKS_HPP_
