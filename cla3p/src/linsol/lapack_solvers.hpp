9932a37b46587fc7e78e4078a918464000fc5de4
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-03-20 20:43:11 +0200

    Add new files

---
+++ cla3p/src/linsol/lapack_solvers.hpp
@@ -0,0 +1,20 @@
+#ifndef CLA3P_LAPACK_SOLVERS_HPP_
+#define CLA3P_LAPACK_SOLVERS_HPP_
+
+/**
+ * @file
+ * Lapack linear solvers
+ */
+
+//#include "../types.hpp"
+//#include "../dense.hpp"
+
+/*-------------------------------------------------*/
+namespace cla3p { 
+/*-------------------------------------------------*/
+
+/*-------------------------------------------------*/
+} // namespace cla3p
+/*-------------------------------------------------*/
+
+#endif // CLA3P_LAPACK_SOLVERS_HPP_
