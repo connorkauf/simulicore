74e4722c68c9c308a83e594c4ba7ecb3fa0af728
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-03-24 15:10:17 +0200

    Add dense LU solver

---
+++ cla3p/src/linsol/dns_lu_lsolver.cpp
@@ -0,0 +1,126 @@
+// this file inc
+#include "dns_lu_lsolver.hpp"
+
+// system
+
+// 3rd
+
+// cla3p
+#include "../dense.hpp"
+#include "../proxies/lapack_proxy.hpp"
+#include "../support/error.hpp"
+#include "../checks/all_checks.hpp"
+
+/*-------------------------------------------------*/
+namespace cla3p {
+namespace dns {
+/*-------------------------------------------------*/
+template <typename T>
+LSolverLU<T>::LSolverLU()
+{
+}
+/*-------------------------------------------------*/
+template <typename T>
+LSolverLU<T>::LSolverLU(uint_t n)
+{
+	reserve(n);
+}
+/*-------------------------------------------------*/
+template <typename T>
+LSolverLU<T>::~LSolverLU()
+{
+	this->clear();
+}
+/*-------------------------------------------------*/
+template <typename T>
+void LSolverLU<T>::reserve(uint_t n)
+{
+	this->reserveBuffer(n);
+	this->reserveIpiv(n);
+}
+/*-------------------------------------------------*/
+template <typename T>
+void LSolverLU<T>::decompose(const T& mat)
+{
+	this->factor().clear();
+	lu_decomp_input_check(mat);
+	this->absorbInput(mat);
+	fdecompose();
+}
+/*-------------------------------------------------*/
+template <typename T>
+void LSolverLU<T>::idecompose(T& mat)
+{
+	this->factor().clear();
+	lu_decomp_input_check(mat);
+	this->factor() = mat.move();
+	fdecompose();
+}
+/*-------------------------------------------------*/
+template <typename T>
+void LSolverLU<T>::solve(T& rhs) const
+{
+	default_solve_input_check(rhs);
+
+	if(this->factor().empty()) {
+		throw InvalidOp("Decomposition stage is not performed");
+	} // empty factor
+
+	int_t info = 0;
+
+	if(this->factor().prop().isGeneral()) {
+
+		info = lapack::getrs('N',
+				this->factor().ncols(),
+				rhs.ncols(),
+				this->factor().values(),
+				this->factor().ld(),
+				this->ipiv1().data(),
+				rhs.values(),
+				rhs.ld());
+
+	} else {
+
+		throw Exception("Unreachable");
+
+	} // prop
+
+	lapack_info_check(info);
+}
+/*-------------------------------------------------*/
+template <typename T>
+void LSolverLU<T>::fdecompose()
+{
+	this->factor().igeneral();
+
+	if(this->factor().prop().isGeneral()) {
+
+		this->ipiv1().resize(std::min(this->factor().nrows(), this->factor().ncols()));
+
+		this->info() = lapack::getrf(
+				this->factor().nrows(),
+				this->factor().ncols(),
+				this->factor().values(),
+				this->factor().ld(),
+				this->ipiv1().data());
+
+	} else {
+
+		throw Exception("Unreachable");
+
+	} // prop
+
+	lapack_info_check(this->info());
+}
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+template class LSolverLU<RdMatrix>;
+template class LSolverLU<RfMatrix>;
+template class LSolverLU<CdMatrix>;
+template class LSolverLU<CfMatrix>;
+/*-------------------------------------------------*/
+} // namespace dns
+} // namespace cla3p
+/*-------------------------------------------------*/
+
