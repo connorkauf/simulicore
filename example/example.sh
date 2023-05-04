a64dc70c4c0ca254a29cb427690b240bfb1abe92
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-05-04 15:56:11 +0300

    First commit after format
      updated MKL to 2023.1.0

--- example/example.sh
+++ example/example.sh
@@ -4,8 +4,8 @@ ROOT_DIR=`dirname $0`
 CLA3P_LIB=${ROOT_DIR}/../lib
 CMPI_LIB=${ROOT_DIR}/../lib
 
-MKL_LIB=/home/konstantinos/Development/3rd/intel/oneapi/mkl/2023.0.0/lib/intel64
-IOMP5_LIB=/home/konstantinos/Development/3rd/intel/oneapi/compiler/2023.0.0/linux/compiler/lib/intel64_lin
+MKL_LIB=/home/connor/Development/3rd/intel/mkl_23.1.0/lib/intel64
+IOMP5_LIB=/home/connor/Development/3rd/intel/compiler_23.1.0/lib/intel64_lin
 
 if [[ -z ${LD_LIBRARY_PATH} ]]; then
 	export LD_LIBRARY_PATH=${CLA3P_LIB}:${CMPI_LIB}:${MKL_LIB}:${IOMP5_LIB}
