310fc937ad0eb47fdc27ed3fb3c036fe69ba0bfd
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-07-06 13:46:56 +0300

    External i32/64 option in config Add sample readme file

--- config.sh
+++ config.sh
@@ -3,5 +3,5 @@ ROOT_DIR=`dirname $0`
 rm -rf ${ROOT_DIR}/build/*
 rm -rf ${ROOT_DIR}/build/build.ninja_*
 rm -rf ${ROOT_DIR}/install/*
-cmake -GNinja -S ${ROOT_DIR} -B ${ROOT_DIR}/build
+cmake -GNinja -S ${ROOT_DIR} -B ${ROOT_DIR}/build $@
 exit 0
