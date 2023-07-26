7bb0e1e63f8bc11e837f6ed483d77e5b5b60bdd9
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-07-26 13:23:43 +0300

    Replace 'Array2D<T_Scalar>::' with 'this->' in XxObject

--- cla3p/src/dense/dns_xxobject.cpp
+++ cla3p/src/dense/dns_xxobject.cpp
@@ -37,7 +37,7 @@ XxObjectTlst
 T_ReturnType XxObjectTmpl::copy() const
 {
 	T_ReturnType ret;
-	Array2D<T_Scalar>::copyTo(ret);
+	this->copyTo(ret);
 	return ret;
 }
 /*-------------------------------------------------*/
@@ -45,7 +45,7 @@ XxObjectTlst
 T_ReturnType XxObjectTmpl::rcopy()
 {
 	T_ReturnType ret;
-	Array2D<T_Scalar>::copyToShallow(ret);
+	this->copyToShallow(ret);
 	return ret;
 }
 /*-------------------------------------------------*/
@@ -62,7 +62,7 @@ XxObjectTlst
 T_ReturnType XxObjectTmpl::move()
 {
 	T_ReturnType ret;
-	Array2D<T_Scalar>::moveTo(ret);
+	this->moveTo(ret);
 	return ret;
 }
 /*-------------------------------------------------*/
@@ -70,35 +70,35 @@ XxObjectTlst
 void XxObjectTmpl::scale(T_Scalar val)
 {
 	bulk::dns::scale(
-			Array2D<T_Scalar>::property().uplo(), 
-			Array2D<T_Scalar>::rsize(), 
-			Array2D<T_Scalar>::csize(), 
-			Array2D<T_Scalar>::values(), 
-			Array2D<T_Scalar>::lsize(), val);
+			this->property().uplo(), 
+			this->rsize(), 
+			this->csize(), 
+			this->values(), 
+			this->lsize(), val);
 }
 /*-------------------------------------------------*/
 XxObjectTlst
 T_RScalar XxObjectTmpl::normOne() const
 { 
 	return bulk::dns::norm_one(
-			Array2D<T_Scalar>::property().type(),
-			Array2D<T_Scalar>::property().uplo(), 
-			Array2D<T_Scalar>::rsize(), 
-			Array2D<T_Scalar>::csize(), 
-			Array2D<T_Scalar>::values(), 
-			Array2D<T_Scalar>::lsize());
+			this->property().type(),
+			this->property().uplo(), 
+			this->rsize(), 
+			this->csize(), 
+			this->values(), 
+			this->lsize());
 }
 /*-------------------------------------------------*/
 XxObjectTlst
 T_RScalar XxObjectTmpl::normInf() const
 { 
 	return bulk::dns::norm_inf(
-			Array2D<T_Scalar>::property().type(),
-			Array2D<T_Scalar>::property().uplo(), 
-			Array2D<T_Scalar>::rsize(), 
-			Array2D<T_Scalar>::csize(), 
-			Array2D<T_Scalar>::values(), 
-			Array2D<T_Scalar>::lsize());
+			this->property().type(),
+			this->property().uplo(), 
+			this->rsize(), 
+			this->csize(), 
+			this->values(), 
+			this->lsize());
 }
 /*-------------------------------------------------*/
 XxObjectTlst
@@ -112,18 +112,18 @@ XxObjectTlst
 T_ReturnType XxObjectTmpl::getBlockReference(uint_t ibgn, uint_t jbgn, uint_t ni, uint_t nj)
 {
 	block_op_consistency_check(
-			Array2D<T_Scalar>::property(),
-			Array2D<T_Scalar>::rsize(),
-			Array2D<T_Scalar>::csize(),
+			this->property(),
+			this->rsize(),
+			this->csize(),
 			ibgn, jbgn, ni, nj);
 	
 	T_Scalar *p_vij = bulk::dns::ptrmv(
-			Array2D<T_Scalar>::lsize(),
-			Array2D<T_Scalar>::values(),
+			this->lsize(),
+			this->values(),
 			ibgn, jbgn);
 	
 	T_ReturnType ret;
-	ret.wrapper(ni, nj, Array2D<T_Scalar>::lsize(), p_vij, false, Array2D<T_Scalar>::property());
+	ret.wrapper(ni, nj, this->lsize(), p_vij, false, this->property());
 	return ret;
 }
 /*-------------------------------------------------*/
