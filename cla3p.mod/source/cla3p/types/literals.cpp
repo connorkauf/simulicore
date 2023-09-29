171f77186dc67998ee0250b708a3ae9e0285dca9
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-09-29 17:37:36 +0300

    Replace line-programming with more formal style

--- cla3p.mod/source/cla3p/types/literals.cpp
+++ cla3p.mod/source/cla3p/types/literals.cpp
@@ -10,34 +10,133 @@
 /*-------------------------------------------------*/
 namespace cla3p {
 /*-------------------------------------------------*/
-std::string stringInteger()           { return "Integer"; }
-std::string stringUnsignedInteger()   { return "Unsigned " + stringInteger(); }
-std::string stringReal()              { return "Real"; }
-std::string stringComplex()           { return "Complex"; }
-std::string stringSingle()            { return "Single (32bit)"; }
-std::string stringDouble()            { return "Double (64bit)"; }
-std::string stringDense()             { return "Dense"; }
-std::string stringSparseCsc()         { return "Sparse (csc)"; }
-std::string stringVector()            { return "Vector"; }
-std::string stringMatrix()            { return "Matrix"; }
-std::string stringPermutationMatrix() { return "Permutation " + stringMatrix(); }
-std::string stringDenseVector()       { return stringDense() + " " + stringVector(); }
-std::string stringDenseMatrix ()      { return stringDense() + " " + stringMatrix(); }
+std::string stringInteger()
+{ 
+	return "Integer"; 
+}
 /*-------------------------------------------------*/
-std::string stringUnknown() { return "Unknown"; }
+std::string stringUnsignedInteger()
+{ 
+	return "Unsigned " + stringInteger(); 
+}
 /*-------------------------------------------------*/
-std::string stringNoOperation()                 { return "No operation";}
-std::string stringTransposeOperation()          { return "Transpose";}
-std::string stringConjugateTransposeOperation() { return "Conjugate Transpose";}
+std::string stringReal()
+{ 
+	return "Real"; 
+}
 /*-------------------------------------------------*/
-std::string stringGeneral()    { return "General"; }
-std::string stringSymmetric()  { return "Symmetric"; }
-std::string stringHermitian()  { return "Hermitian"; }
-std::string stringTriangular() { return "Triangular/Trapezoidal"; }
-std::string stringSkew()       { return "Skew"; }
+std::string stringComplex()
+{ 
+	return "Complex"; 
+}
 /*-------------------------------------------------*/
-std::string stringUpper() { return "Upper"; }
-std::string stringLower() { return "Lower"; }
+std::string stringSingle()
+{ 
+	return "Single (32bit)"; 
+}
+/*-------------------------------------------------*/
+std::string stringDouble()
+{ 
+	return "Double (64bit)"; 
+}
+/*-------------------------------------------------*/
+std::string stringDense()
+{ 
+	return "Dense"; 
+}
+/*-------------------------------------------------*/
+std::string stringSparseCsc()
+{ 
+	return "Sparse (csc)"; 
+}
+/*-------------------------------------------------*/
+std::string stringVector()
+{ 
+	return "Vector"; 
+}
+/*-------------------------------------------------*/
+std::string stringMatrix()
+{ 
+	return "Matrix"; 
+}
+/*-------------------------------------------------*/
+std::string stringPermutationMatrix()
+{ 
+	return "Permutation " + stringMatrix(); 
+}
+/*-------------------------------------------------*/
+std::string stringDenseVector()
+{ 
+	return stringDense() + " " + stringVector(); 
+}
+/*-------------------------------------------------*/
+std::string stringDenseMatrix ()
+{ 
+	return stringDense() + " " + stringMatrix(); 
+}
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+std::string stringUnknown()
+{ 
+	return "Unknown"; 
+}
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+std::string stringNoOperation()
+{ 
+	return "No operation";
+}
+/*-------------------------------------------------*/
+std::string stringTransposeOperation()
+{ 
+	return "Transpose";
+}
+/*-------------------------------------------------*/
+std::string stringConjugateTransposeOperation()
+{ 
+	return "Conjugate Transpose";
+}
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+std::string stringGeneral()
+{ 
+	return "General"; 
+}
+/*-------------------------------------------------*/
+std::string stringSymmetric()
+{ 
+	return "Symmetric"; 
+}
+/*-------------------------------------------------*/
+std::string stringHermitian()
+{ 
+	return "Hermitian"; 
+}
+/*-------------------------------------------------*/
+std::string stringTriangular()
+{ 
+	return "Triangular/Trapezoidal"; 
+}
+/*-------------------------------------------------*/
+std::string stringSkew()
+{ 
+	return "Skew"; 
+}
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+std::string stringUpper()
+{ 
+	return "Upper"; 
+}
+/*-------------------------------------------------*/
+std::string stringLower()
+{ 
+	return "Lower"; 
+}
 /*-------------------------------------------------*/
 } // namespace cla3p
 /*-------------------------------------------------*/
