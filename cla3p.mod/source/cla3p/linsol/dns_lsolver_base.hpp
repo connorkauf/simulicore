1ae9ee02a2405de76bab8b5952f5ce4dce356f51
Connor Kaufman <connor.kaufman.gh@outlook.com>
2023-11-02 17:44:33 +0200

    Remove "(c)" from copyright file header

--- cla3p.mod/source/cla3p/linsol/dns_lsolver_base.hpp
+++ cla3p.mod/source/cla3p/linsol/dns_lsolver_base.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Connor C. Kaufman (connor.kaufman.gh@outlook.com)
+ * Copyright 2023-2024 Connor C. Kaufman (connor.kaufman.gh@outlook.com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
