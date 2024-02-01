86c396dcf524f183a591da2d8f16ddaa16630fc5
Connor Kaufman <connor.kaufman.gh@outlook.com>
2024-02-02 01:30:04 +0200

    Add mkl sparse wrappers for csc algebra
      csc plus csc
      csc times dense vec
      csc times dense mat

---
+++ cla3p.mod/source/cla3p/bulk/csc_math.hpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023-2024 Connor C. Kaufman (connor.kaufman.gh@outlook.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CLA3P_BULK_CSC_MATH_HPP_
+#define CLA3P_BULK_CSC_MATH_HPP_
+
+#include "cla3p/types.hpp"
+
+/*-------------------------------------------------*/
+namespace cla3p {
+namespace bulk {
+namespace csc {
+/*-------------------------------------------------*/
+
+//
+// Update: C = alpha * A + B
+// C(m x n)
+//
+template <typename T_Scalar>
+void add(uint_t m, uint_t n, T_Scalar alpha,
+		const int_t *colptrA, const int_t *rowidxA, const T_Scalar *valuesA,
+		const int_t *colptrB, const int_t *rowidxB, const T_Scalar *valuesB,
+		int_t **colptrC, int_t **rowidxC, T_Scalar **valuesC);
+
+/*-------------------------------------------------*/
+} // namespace csc
+} // namespace bulk
+} // namespace cla3p
+/*-------------------------------------------------*/
+
+#endif // CLA3P_BULK_CSC_MATH_HPP_
