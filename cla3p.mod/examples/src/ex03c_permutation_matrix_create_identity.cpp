98e8e8627e358b14c8273729f183e67d53d43283
Connor Kaufman <connor.kaufman.gh@outlook.com>
2024-06-05 11:02:50 +0300

    Update permutation examples
      Show dense representation of permutation matrix in examples

--- cla3p.mod/examples/src/ex03c_permutation_matrix_create_identity.cpp
+++ cla3p.mod/examples/src/ex03c_permutation_matrix_create_identity.cpp
@@ -4,7 +4,18 @@
 
 #include <iostream>
 #include "cla3p/perms.hpp"
+#include "cla3p/dense.hpp"
 
+/*-----------------------------------------------------*/
+cla3p::dns::RdMatrix permToDense(const cla3p::prm::PiMatrix& P)
+{
+	cla3p::dns::RdMatrix ret(P.size(), P.size());
+	ret = 0;
+	for(cla3p::uint_t i = 0; i < P.size(); i++)
+		ret(P(i),i) = cla3p::real_t(1);
+	return ret;
+}
+/*-----------------------------------------------------*/
 int main()
 {
 	/* 
@@ -14,6 +25,7 @@ int main()
 	cla3p::prm::PiMatrix Q = cla3p::prm::PiMatrix::identity(5);
 
 	std::cout << Q.info("Q") << Q;
+	std::cout << "As a dense matrix:\n" << permToDense(Q);
 
 	return 0;
 }
--- cla3p.mod/examples/src/ex03d_permutation_matrix_create_random.cpp
+++ cla3p.mod/examples/src/ex03d_permutation_matrix_create_random.cpp
@@ -4,7 +4,18 @@
 
 #include <iostream>
 #include "cla3p/perms.hpp"
+#include "cla3p/dense.hpp"
 
+/*-----------------------------------------------------*/
+cla3p::dns::RdMatrix permToDense(const cla3p::prm::PiMatrix& P)
+{
+	cla3p::dns::RdMatrix ret(P.size(), P.size());
+	ret = 0;
+	for(cla3p::uint_t i = 0; i < P.size(); i++)
+		ret(P(i),i) = cla3p::real_t(1);
+	return ret;
+}
+/*-----------------------------------------------------*/
 int main()
 {
 	/* 
@@ -15,6 +26,7 @@ int main()
 	cla3p::prm::PiMatrix P = cla3p::prm::PiMatrix::random(4);
 
 	std::cout << P.info("P") << P;
+	std::cout << "As a dense matrix:\n" << permToDense(P);
 
 	return 0;
 }
