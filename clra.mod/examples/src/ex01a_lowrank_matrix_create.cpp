9287d14e48432d4e6146994285c88e78c9468ac5
Connor Kaufman <connor.kaufman.gh@outlook.com>
2024-07-22 16:50:08 +0300

    Filled 1st CLRA example

--- clra.mod/examples/src/ex01a_lowrank_matrix_create.cpp
+++ clra.mod/examples/src/ex01a_lowrank_matrix_create.cpp
@@ -2,7 +2,34 @@
  * @example ex01a_lowrank_matrix_create.cpp
  */
 
+#include <iostream>
+#include "clra/matrix.hpp"
+
 int main()
 {
+	/*
+	 * Double precision real empty matrix
+	 */
+
+	cla3p::lra::RdMatrix A;
+
+	std::cout << A.info("A");
+
+	/*
+	 * Rank 2 (3x4) single precision real matrix (uninitialized values)
+	 */
+
+	cla3p::lra::LrMatrix<cla3p::dns::RfMatrix> B(3,4,2);
+
+	std::cout << B.info("B");
+
+	/*
+	 * Allocate space for A (rank 2, 5x3, uninitialized values)
+	 */
+
+	A = cla3p::lra::RdMatrix::init(5,3,2);
+
+	std::cout << A.info("A");
+
 	return 0;
 }
--- clra.mod/examples/src/ex01b_lowrank_matrix_create_random.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @example ex01b_lowrank_matrix_create_random.cpp
- */
-
-int main()
-{
-	return 0;
-}
